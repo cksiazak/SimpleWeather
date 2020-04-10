fd3d4bdefb8e320af3ca1460a8233c9722a805b2
Christopher Ksiazak <cksiazak@gmail.com>
2020-04-10 04:40:32 -0600

    initial config

---
+++ pages/api/weather.js
@@ -0,0 +1,14 @@
+import fetch from 'isomorphic-unfetch';
+
+export default async (req, res) => {
+  try {
+    const data = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARKSKY}/37.8267,-122.4233`
+    );
+    const weather = await data.json();
+    res.status(200).json(weather);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
